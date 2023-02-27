85e77f09b8fed782c35f671a0fa2fe974048a8ff
root <haixing9@qq.com>
2023-02-27 17:02:52 +0800

    初版文件处理工具，支持文件批量重命名

---
+++ HnoyiTools.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace HnoyiTools
+{
+    partial class HnoyiTools
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HnoyiTools));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SrcPathBox = new System.Windows.Forms.TextBox();
+            this.DestPathBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SrcPathbutton = new System.Windows.Forms.Button();
+            this.DestPathbutton = new System.Windows.Forms.Button();
+            this.MsgBox = new System.Windows.Forms.TextBox();
+            this.BackWorker = new System.ComponentModel.BackgroundWorker();
+            this.StartWorkButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.ProgresLabel = new System.Windows.Forms.Label();
+            this.StopWorkButton = new System.Windows.Forms.Button();
+            this.MoveCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "源文件目录：";
+            // 
+            // SrcPathBox
+            // 
+            this.SrcPathBox.Location = new System.Drawing.Point(137, 36);
+            this.SrcPathBox.Name = "SrcPathBox";
+            this.SrcPathBox.Size = new System.Drawing.Size(174, 21);
+            this.SrcPathBox.TabIndex = 2;
+            // 
+            // DestPathBox
+            // 
+            this.DestPathBox.Location = new System.Drawing.Point(137, 90);
+            this.DestPathBox.Name = "DestPathBox";
+            this.DestPathBox.Size = new System.Drawing.Size(174, 21);
+            this.DestPathBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "新文件目录：";
+            // 
+            // SrcPathbutton
+            // 
+            this.SrcPathbutton.Location = new System.Drawing.Point(329, 34);
+            this.SrcPathbutton.Name = "SrcPathbutton";
+            this.SrcPathbutton.Size = new System.Drawing.Size(75, 23);
+            this.SrcPathbutton.TabIndex = 5;
+            this.SrcPathbutton.Text = "选择文件夹";
+            this.SrcPathbutton.UseVisualStyleBackColor = true;
+            this.SrcPathbutton.Click += new System.EventHandler(this.SrcPathbutton_Click);
+            // 
+            // DestPathbutton
+            // 
+            this.DestPathbutton.Location = new System.Drawing.Point(327, 90);
+            this.DestPathbutton.Name = "DestPathbutton";
+            this.DestPathbutton.Size = new System.Drawing.Size(75, 23);
+            this.DestPathbutton.TabIndex = 6;
+            this.DestPathbutton.Text = "选择文件夹";
+            this.DestPathbutton.UseVisualStyleBackColor = true;
+            this.DestPathbutton.Click += new System.EventHandler(this.DestPathbutton_Click);
+            // 
+            // MsgBox
+            // 
+            this.MsgBox.BackColor = System.Drawing.SystemColors.Info;
+            this.MsgBox.Location = new System.Drawing.Point(54, 238);
+            this.MsgBox.Multiline = true;
+            this.MsgBox.Name = "MsgBox";
+            this.MsgBox.ReadOnly = true;
+            this.MsgBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MsgBox.Size = new System.Drawing.Size(350, 111);
+            this.MsgBox.TabIndex = 7;
+            // 
+            // BackWorker
+            // 
+            this.BackWorker.WorkerReportsProgress = true;
+            this.BackWorker.WorkerSupportsCancellation = true;
+            this.BackWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackWorker_DoWork);
+            this.BackWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackWorker_ProgressChanged);
+            this.BackWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackWorker_RunWorkerCompleted);
+            // 
+            // StartWorkButton
+            // 
+            this.StartWorkButton.Location = new System.Drawing.Point(100, 369);
+            this.StartWorkButton.Name = "StartWorkButton";
+            this.StartWorkButton.Size = new System.Drawing.Size(75, 23);
+            this.StartWorkButton.TabIndex = 8;
+            this.StartWorkButton.Text = "开始整理";
+            this.StartWorkButton.UseVisualStyleBackColor = true;
+            this.StartWorkButton.Click += new System.EventHandler(this.StartWorkButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(54, 198);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(350, 23);
+            this.progressBar.TabIndex = 10;
+            // 
+            // ProgresLabel
+            // 
+            this.ProgresLabel.AutoSize = true;
+            this.ProgresLabel.Location = new System.Drawing.Point(54, 180);
+            this.ProgresLabel.Name = "ProgresLabel";
+            this.ProgresLabel.Size = new System.Drawing.Size(59, 12);
+            this.ProgresLabel.TabIndex = 11;
+            this.ProgresLabel.Text = "处理进度:";
+            // 
+            // StopWorkButton
+            // 
+            this.StopWorkButton.Location = new System.Drawing.Point(252, 369);
+            this.StopWorkButton.Name = "StopWorkButton";
+            this.StopWorkButton.Size = new System.Drawing.Size(75, 23);
+            this.StopWorkButton.TabIndex = 12;
+            this.StopWorkButton.Text = "停止";
+            this.StopWorkButton.UseVisualStyleBackColor = true;
+            this.StopWorkButton.Click += new System.EventHandler(this.StopWorkButton_Click);
+            // 
+            // MoveCheckBox
+            // 
+            this.MoveCheckBox.AutoSize = true;
+            this.MoveCheckBox.Location = new System.Drawing.Point(54, 133);
+            this.MoveCheckBox.Name = "MoveCheckBox";
+            this.MoveCheckBox.Size = new System.Drawing.Size(48, 16);
+            this.MoveCheckBox.TabIndex = 13;
+            this.MoveCheckBox.Text = "移动";
+            this.MoveCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // HnoyiTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 427);
+            this.Controls.Add(this.MoveCheckBox);
+            this.Controls.Add(this.StopWorkButton);
+            this.Controls.Add(this.ProgresLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.StartWorkButton);
+            this.Controls.Add(this.MsgBox);
+            this.Controls.Add(this.DestPathbutton);
+            this.Controls.Add(this.SrcPathbutton);
+            this.Controls.Add(this.DestPathBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SrcPathBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HnoyiTools";
+            this.Text = "HnoyiTools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SrcPathBox;
+        private System.Windows.Forms.TextBox DestPathBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SrcPathbutton;
+        private System.Windows.Forms.Button DestPathbutton;
+        private System.Windows.Forms.TextBox MsgBox;
+        private System.ComponentModel.BackgroundWorker BackWorker;
+        private System.Windows.Forms.Button StartWorkButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label ProgresLabel;
+        private System.Windows.Forms.Button StopWorkButton;
+        private System.Windows.Forms.CheckBox MoveCheckBox;
+    }
+}
+
---
+++ HnoyiTools.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HnoyiTools
+{
+    public partial class HnoyiTools : Form
+    {
+        public HnoyiTools()
+        {
+            InitializeComponent();
+        }
+
+        //记录文件总数，显示进度
+        static long FileNum = 0;
+        static bool MoveEnable = false;
+        //随机数
+        private static Random random = new Random();
+        /// <summary>
+        /// 随机字符串
+        /// </summary>
+        /// <param name="chars"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public string GetRandomStr(string chars, int length)
+        {
+            if (string.IsNullOrEmpty(chars))
+            {
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghizklmnopqrstuvwxyz0123456789";
+            }
+            //const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void SrcPathbutton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择文件所在文件夹目录";  //提示的文字
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                SrcPathBox.Text = folder.SelectedPath;
+            }
+
+        }
+
+        private void DestPathbutton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择文件所在文件夹目录";  //提示的文字
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                DestPathBox.Text = folder.SelectedPath;
+            }
+        }
+
+        private void BackWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // 遍历所有文件
+            var files = Directory.GetFiles(SrcPathBox.Text, "*.*", SearchOption.AllDirectories);
+            int i = 0;
+
+            foreach (var file in files)
+            {
+                //判断退出
+                if (BackWorker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                //获取文件创建时间等信息
+                FileInfo fi = new FileInfo(file);
+
+                /*
+                Console.WriteLine("创建时间:" + fi.CreationTime);
+                Console.WriteLine("修改时间:" + fi.LastWriteTime);
+                Console.WriteLine("访问时间:" + fi.LastAccessTime);
+                */
+                //取最早时间，copy的文件创建时间会很新
+                DateTime Time = fi.CreationTime;
+                if (DateTime.Compare(fi.CreationTime, fi.LastWriteTime) > 0)
+                {
+                    Time = fi.LastWriteTime;
+                }
+
+                //按年月方式创建目录
+                string DestPath = DestPathBox.Text + @"\" + Time.Year + @"\" + Time.Month + @"\";
+                if (!Directory.Exists(DestPath))
+                {
+                    Directory.CreateDirectory(DestPath);
+                }
+
+                //重命名文件
+                string SourceFile = file;
+                string DestFile = DestPath + Time.ToString("yyyy-MM-dd__HH_mm_ss") + fi.Extension;
+
+                //防止重名
+                if (File.Exists(DestFile))
+                {
+                    DestFile = DestPath + Time.ToString("yyyy-MM-dd__HH_mm_ss") + "_" + GetRandomStr(null, 4) + fi.Extension;
+                }
+
+                //移动文件
+                if (MoveEnable)
+                {
+                    File.Move(SourceFile, DestFile);
+                }
+                else
+                {
+                    //默认复制文件
+                    File.Copy(SourceFile, DestFile);
+                }
+
+
+                //通知进度
+                string ProcessMsg = "原文件:" + SourceFile + "\r\n" + "新文件" + DestFile + "\r\n";
+                //Console.WriteLine(ProcessMsg);
+
+                i++;
+                BackWorker.ReportProgress(i, ProcessMsg);
+            }
+
+        }
+
+        private void BackWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //BackWorker.ReportProgress方法传递过来的参数userState
+            string state = (string)e.UserState;
+            progressBar.Value = e.ProgressPercentage;
+            ProgresLabel.Text = "处理进度:" + Convert.ToString(e.ProgressPercentage) + "/" + FileNum;
+            MsgBox.Text += e.UserState.ToString();
+        }
+
+        private void BackWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //计算过程中的异常会被抓住，在这里可以进行处理。
+            //这里没做异常处理
+            if (e.Error != null)
+            {
+                Type errorType = e.Error.GetType();
+                switch (errorType.Name)
+                {
+                    case "ArgumentNullException":
+                    case "MyException":
+                        //do something.
+                        break;
+                    default:
+                        //do something.
+                        break;
+                }
+            }
+        }
+
+        private void StartWorkButton_Click(object sender, EventArgs e)
+        {
+            //判断是否正在运行异步操作
+            if (BackWorker.IsBusy)
+                return;
+
+            if (SrcPathBox.Text.Length < 1 || DestPathBox.Text.Length < 1)
+            {
+                MessageBox.Show("请选择源文件目录和目标文件夹目录！");
+                return;
+            }
+            //
+            //判断文件操作模式
+            if (MoveCheckBox.Checked)
+            {
+                MoveEnable = true;
+            }
+
+            //清空显示
+            ProgresLabel.Text = "处理进度:";
+            MsgBox.Text = "";
+
+            //填充总文件数表示进度
+            FileNum = progressBar.Maximum = Directory.GetFiles(SrcPathBox.Text, "*.*", SearchOption.AllDirectories).Length;
+
+            //给业务传递参数，就是把前台的要处理的参数传递进去
+            BackWorker.RunWorkerAsync();
+
+            StopWorkButton.Enabled = true;
+        }
+
+        private void StopWorkButton_Click(object sender, EventArgs e)
+        {
+            //取消后台进程
+            BackWorker.CancelAsync();
+        }
+    }
+}
